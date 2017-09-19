8702d31b5481353740d8f52daaa90da369c88f0e
Shaun Ellis <shaun@sdellis.com>
2017-09-19 17:26:18 -0400

    full refactor. fixes #6

--- src/main.js
+++ src/main.js
@@ -1,24 +1,12 @@
 import img_collection from './data/img_collection'
 import fmu from './components/FilemanagerUI';
 
-$('#sidebar').affix({
-  offset: {
-    bottom: 235
-  }
-});
 
 var navHeight = $('.navbar').outerHeight(true) + 10;
 
 $(function() {
 
   $("#filemanager").filemanager({ images: img_collection })
-
-  // draw plugin to dom >>>
-  // var folder = $( "<div></div>" )
-  //     .appendTo( "body" )
-  //     .filemanager({
-  //         images: img_collection
-  //     })
 
   // listen for the objectSaved event to catch and persist the data
   $( "body" ).on( "objectSaved", function( event, img_collection ) {
